b7adc13ce8ffe652ac591a818a3b418d19d71f85
Darpandeep Kaur <darpansandhu195@gmail.com>
2022-07-11 16:47:30 -0700

    adding source code and package.json

---
+++ index.js
@@ -0,0 +1,16 @@
+import { createRequire } from "module";
+const require = createRequire( import.meta.url);
+
+import {franc} from "franc";
+const langs = require("langs");
+const colors = require("colors");
+
+const input = process.argv[2];
+const langCode = franc(input);
+
+if (langCode === "und") {
+    console.log("SORRY, COULDN'T FIGURE IT OUT! TRY WITH A LONGER SENTENCE".red)
+} else {
+    const language = langs.where("3", langCode);
+    console.log(`Our best guess is: ${language.name.green}`);
+}
